f68d5e3efcef48e6599f3795b87cd344edd88b62
Graziela Pauselius <grazielak.pauselius@gmail.com>
2022-09-01 08:47:59 -0700

    Create develop branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [pokemon, setPokemon] = useState(null);
+	useEffect(() => {
+		const getPokemon = async () => {
+			try {
+				const response = await axios.get(`http://localhost:3000/pokemon`);
+				// console.log(response.data.pokemonData);
+				const pokemonData = await response.data.pokemonData;
+				console.log(pokemonData);
+				setPokemon(pokemonData);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getPokemon();
+	}, []);
+
+	if (pokemon === null) {
+		return <h1>Loding...</h1>;
+	}
+
+	return (
+		<img
+			src={process.env.PUBLIC_URL + "/" + "image" + "/" + pokemon[0].image}
+		/>
+	);
+};
 
 export default App;
